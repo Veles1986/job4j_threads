0d80ace488978cb1f8038746125b1965aa16b29b
Veles1986 <ilyos.rafa@mail.com>
2025-03-06 03:27:00 +0500

    5. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,27 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    @Override
+    public void run() {
+        var process = new char[] {'-', '\\', '|', '/'};
+        int index = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.print("\rLoading... " + process[index]);
+            index = (index + 1) % process.length;
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(5000); // Симуляция выполнения задачи 5 секунд
+        progress.interrupt(); // Останавливаем поток
+        System.out.println("\nЗагрузка завершена!");
+    }
+}
