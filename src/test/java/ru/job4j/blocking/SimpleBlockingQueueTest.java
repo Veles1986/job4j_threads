26a444a0a9852cd9b0fea140634656d7fc494754
Veles1986 <ilyos.rafa@mail.com>
2025-03-21 00:00:34 +0500

    3. Junit тест для блокирующей очереди. [#68589]

--- src/test/java/ru/job4j/blocking/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/blocking/SimpleBlockingQueueTest.java
@@ -2,6 +2,8 @@ package ru.job4j.blocking;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.CopyOnWriteArrayList;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class SimpleBlockingQueueTest {
@@ -39,4 +41,38 @@ class SimpleBlockingQueueTest {
         assertThat(queue.isEmpty()).isTrue();
     }
 
+    @Test
+    void whenBlockingTestThenOk() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        Thread producer = new Thread(
+                () -> {
+                    for (int i = 0; i < 20; i++) {
+                        try {
+                            queue.offer(i);
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer).containsExactly(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19);
+    }
 }
