5eb093a3a23f58ca8ddd2fbadb99c229acafafc7
Veles1986 <ilyos.rafa@mail.com>
2025-03-27 00:09:40 +0500

    1. Неблокирующий кеш [#4741 #537107]

--- src/main/java/ru/job4j/cache/Cache.java
+++ src/main/java/ru/job4j/cache/Cache.java
@@ -1,10 +1,8 @@
 package ru.job4j.cache;
 
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Stream;
 
 public class Cache {
     private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
@@ -16,18 +14,16 @@ public class Cache {
         return memory.putIfAbsent(model.id(), model) == null;
     }
 
-    public boolean update(Base model) throws OptimisticException {
+    public boolean update(Base model) {
         if (model == null) {
             throw new IllegalArgumentException("Model cannot be null");
         }
-        Base stored = memory.get(model.id());
-        if (stored == null) {
-            return false;
-        }
-        if (stored.version() != model.version()) {
-            throw new OptimisticException("Versions are not equal");
-        }
-        return memory.replace(model.id(), stored, new Base(model.id(), model.name(), model.version() + 1));
+        return memory.computeIfPresent(model.id(), (id, stored) -> {
+            if (stored.version() != model.version()) {
+                throw new OptimisticException("Versions are not equal");
+            }
+            return new Base(model.id(), model.name(), model.version() + 1);
+        }) != null;
     }
 
     public void delete(int id) {
@@ -35,8 +31,6 @@ public class Cache {
     }
 
     public Optional<Base> findById(int id) {
-        return Stream.of(memory.get(id))
-                .filter(Objects::nonNull)
-                .findFirst();
+        return Optional.ofNullable(memory.get(id));
     }
 }
