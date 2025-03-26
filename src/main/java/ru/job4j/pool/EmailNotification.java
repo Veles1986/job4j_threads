d4a0c30b1af1fe68c882684e499ef58712daae6a
Veles1986 <ilyos.rafa@mail.com>
2025-03-27 01:36:01 +0500

    2. ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/pool/EmailNotification.java
@@ -0,0 +1,24 @@
+package ru.job4j.pool;
+
+import java.util.concurrent.*;
+
+public class EmailNotification {
+    private final ExecutorService executorService = Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors()
+    );
+
+    public void emailTo(User user) {
+        executorService.submit(() -> {
+            String subject = String.format("Notification %s to email %s", user.username(), user.email());
+            String body = String.format("Add a new event to %s", user.username());
+            send(subject, body, user.email());
+        });
+    }
+
+    public void send(String subject, String body, String email) {
+    }
+
+    public void close() {
+        executorService.shutdown();
+    }
+}
